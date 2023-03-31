2862599c4145ce51ca9d7585536a61b7266dc8e9
mela980407 <56041188+mela980407@users.noreply.github.com>
2023-03-30 20:32:36 -0500

    Update typeorm-db.js

--- typeorm-db.js
+++ typeorm-db.js
@@ -2,14 +2,15 @@ var typeorm = require("typeorm");
 var EntitySchema = typeorm.EntitySchema;
 
 const Users = require("./entity/Users")
-
+const usuario = ["root"];
+const contrasena = ["root"];
 typeorm.createConnection({
   name: "mysql",
   type: "mysql",
   host: "localhost",
   port: 3306,
-  username: "root",
-  password: "root",
+  username: usuario,
+  password: contrasena,
   database: "acme",
   synchronize: true,
   "logging": true,
