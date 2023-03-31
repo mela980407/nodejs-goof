b1743150a7f79bf4550db37f9483dc99a1c08c4f
mela980407 <56041188+mela980407@users.noreply.github.com>
2023-03-30 19:41:15 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -60,7 +60,7 @@ function adminLoginSuccess(redirectPage, session, username, res) {
   console.log(`User logged in: ${username}`)
 
   if (redirectPage) {
-      return res.redirect(redirectPage+'/valid')
+      return res.redirect(redirectPage)
   } else {
       return res.redirect('/admin')
   }
